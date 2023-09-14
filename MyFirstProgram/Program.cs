0463dea1aa964290943d68e3b4ff7a8a368d0e41
Alexander Doja <alexander.doja@outlook.com>
2023-09-14 10:49:30 +0200

    Added comments to MyFirstProgram

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -9,19 +9,24 @@ namespace MyFirstProgram
         {
             int num = 39;
 
+            // Check if the number is above 10.
             if (num > 10)
             {
                 Console.WriteLine("Talet är stort!\n");
             } else
             {
+                // If the number is not above 10 this code is executed.
                 Console.WriteLine("Oj. Lågt tal!\n");  
             }
 
+            // Ask the user for input.
             Console.WriteLine("Vad heter du?");
             string name = Console.ReadLine();
 
+            // The string entered by the user is printed to console.
             Console.WriteLine($"Hej {name}!\n");
             
+            // Print every number from 0 up to the number num.
             for (int i = 0; i <= num; i++)
             {
                 Console.WriteLine(i);
