244b08cd7f5d4a78944113371d9b2aa4cf862400
Alexander Doja <alexander.doja@outlook.com>
2023-09-14 11:19:01 +0200

    Created initial ChessBoard

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter a number: ");
+int num =  Int32.Parse(Console.ReadLine());
+
+// Override default newline behaviour.
+Console.Out.NewLine = "";
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < num; i++)
+{
+    for (int j = 0; j < num; j++)
+    {
+        if ((i + j) % 2 == 0)
+        {
+            Console.WriteLine("◼︎ ");
+        }
+        else
+        {
+            Console.WriteLine("◻︎ ");
+        }
+    }
+
+    Console.WriteLine("\n");
+}
