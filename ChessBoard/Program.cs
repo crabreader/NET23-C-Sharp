404ac85f7675a9861b2180dd0c207389a016c8cb
Alexander Doja <alexander.doja@outlook.com>
2023-09-18 12:09:38 +0200

    Worked with methods

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,25 +1,32 @@
-﻿Console.WriteLine("What size should the chessboard be? (enter a number): ");
-int num =  Int32.Parse(Console.ReadLine());
-
-// Override the default newline behaviour.
-Console.Out.NewLine = "";
-
-// Newline for readability.
-Console.WriteLine("\n");
-
-for (int i = 0; i < num; i++)
+﻿internal class Program
 {
-    for (int j = 0; j < num; j++)
+    private static void Main(string[] args)
     {
-        if ((i + j) % 2 == 0)
+        // Console.WriteLine("What size should the chessboard be? (enter a number): ");
+        // int num = int.Parse(Console.ReadLine());
+        int num = int.Parse(args[0]);
+
+        // Override the default newline behaviour.
+        Console.Out.NewLine = "";
+
+        // Newline for readability.
+        Console.WriteLine("\n");
+
+        for (int i = 0; i < num; i++)
         {
-            Console.WriteLine("◼︎ ");
-        }
-        else
-        {
-            Console.WriteLine("◻︎ ");
+            for (int j = 0; j < num; j++)
+            {
+                if ((i + j) % 2 == 0)
+                {
+                    Console.WriteLine("◼︎ ");
+                }
+                else
+                {
+                    Console.WriteLine("◻︎ ");
+                }
+            }
+
+            Console.WriteLine("\n");
         }
     }
-
-    Console.WriteLine("\n");
 }
---
+++ MethodDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace MethodDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Program.Print("Jiiku Hairu!");       
+
+            Program jin = new Program();
+            jin.PrintEvenNumbers(5);
+
+            Program.Print("Jiiku Hairu!");
+            Program.PrintOddNumbers(5);
+        }
+
+        public static void Print(string s) 
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void PrintOddNumbers(int limit)
+        {
+            int start = 1;
+            while (start <= limit)
+            {
+                Console.WriteLine(start);
+                start += 2;
+            }
+        }
+
+        public void PrintEvenNumbers(int limit) 
+        {
+            int i = 0;
+            while (i <= limit)
+            {
+                Console.WriteLine(i);
+                i += 2;
+            }
+        }        
+    }
+}
