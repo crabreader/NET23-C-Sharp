9f634b7e8b05d7f974f5be2456faa2aa766d3225
Alexander Doja <alexander.doja@outlook.com>
2023-09-22 14:02:02 +0200

    Created minimal number guessing game

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 20);
+            int userGuess;
+            bool guessed = false;
+
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+
+            for (int i = 0; (i < 5) && !guessed; i++)
+            {
+                userGuess = GetUserGuess();
+
+                if (GuessIsCorrect(number, userGuess))
+                {
+                    guessed = true;
+                    Console.WriteLine("Wohoo! Du klarade det!");
+                }
+                else
+                {
+                    if (userGuess < number)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för lågt!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för högt!");
+                    }
+                }
+            }
+
+            if (!guessed)
+            {
+                Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
+            }
+        }
+
+        public static int GetUserGuess()
+        {
+            int parsedInput;
+            bool success = int.TryParse(Console.ReadLine(), out parsedInput);
+
+            return parsedInput;
+        }
+
+        public static bool GuessIsCorrect(int number, int guess)
+        {
+            if (number == guess)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
