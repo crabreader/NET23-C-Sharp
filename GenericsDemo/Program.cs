711de5bbaaee2298c7ab131c339062d406660fda
Alexander Doja <alexander.doja@outlook.com>
2023-10-24 12:41:53 +0200

    Generics

---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace GenericsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = Check<double>.GenericIsEqual(5, 2.3);
+            bool result2 = Check<string>.GenericIsEqual("a", "b");
+            if (result)
+            {
+                Console.WriteLine("Is equal");
+            }
+            else
+            {
+                Console.WriteLine("Is not equal");
+            }
+        }
+    }
+
+    public class Check<T>
+    {
+        public T _field;
+
+         public static bool IsEqual(int value1, int value2)
+        {
+            return value1 == value2;
+        }
+
+        public static bool IsEqual(string value1, string value2)
+        {
+            return value1 == value2;
+        }
+
+        public static bool GenericIsEqual(T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+    }
+}
---
+++ ListDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ListDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> myList = new List<int>() { 11, 10, 20, 30, 40, 50, 10};
+
+            myList.Add(150);
+
+            myList.Remove(10);
+
+            myList.RemoveAt(0);
+
+            myList.RemoveRange(4, 2);
+
+            //myList.Clear();
+
+            int result = myList.Count;
+            Console.WriteLine("Count result: " +result);
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Student S1 = new Student();
+            S1.Id = 1;
+            S1.Name = "Aldor";
+            S1.Class = "NET23";
+
+            Student S2 = new Student();
+            S2.Id = 2;
+            S2.Name = "Reidar";
+            S2.Class = "NET23";
+
+            List<Student> students = new List<Student>();
+
+            students.Add(S1);
+            students.Add(S2);
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"id: {student.Id}\nname: {student.Name}\nclass: {student.Class}");
+            }
+
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Class { get; set; }
+
+
+    }
+}
